34603cf47e3bd605c30b745419db8c3d2b102db6
Collins Scott <107673661+Ssersda@users.noreply.github.com>
2022-10-02 10:24:43 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,18 +1,25 @@
 var body = $response.body
 var obj = JSON.parse(body)
- 
+
+function formatDate(time = +new Date()) {
+  var date = new Date(time + 8 * 3600 * 1000) // 增加8小时
+  return date.toJSON().substr(0, 19).replace('T', ' ')
+}
+
 //过期标志
 obj.obj.expireFlag = false
 
 obj.obj.nucleicInfo.expireHour = 48
 //过期时间
-// obj.obj.nucleicInfo.expireTime = '2022-10-15 22:34:32'
-obj.obj.nucleicInfo.expireTimeStamp = '1665844472000'
+const myExpireTimeStamp = Date.now() + 1000 * 60 * 60 * 24 * 1.5
+obj.obj.nucleicInfo.expireTime = formatDate(myExpireTimeStamp)
+obj.obj.nucleicInfo.expireTimeStamp = myExpireTimeStamp.toString()
 
 //采样时间
-// obj.obj.nucleicInfo.samplingTime = '2022-10-1 16:57:52'
-obj.obj.nucleicInfo.sampleTimestamp = (Date.now() - 1254930313).toString()
 
+const mySampleTime = Date.now() - 1000 * 60 * 60 * 24 * 1.5
+obj.obj.nucleicInfo.samplingTime = formatDate(mySampleTime)
+obj.obj.nucleicInfo.sampleTimestamp = mySampleTime.toString()
 
 obj.obj.vaccineInfo.vaccinateCount = 4
 
