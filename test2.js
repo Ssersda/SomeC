4bb9d5f4086eddf375855c63d789fc550dc65640
Collins Scott <107673661+Ssersda@users.noreply.github.com>
2022-10-02 10:27:24 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ var body = $response.body
 var obj = JSON.parse(body)
 
 function formatDate(time = +new Date()) {
-  var date = new Date(time + 8 * 3600 * 1000) // 增加8小时
+  var date = new Date(time + 8 * 3600 * 1000)
   return date.toJSON().substr(0, 19).replace('T', ' ')
 }
 
@@ -11,13 +11,13 @@ obj.obj.expireFlag = false
 
 obj.obj.nucleicInfo.expireHour = 48
 //过期时间
-const myExpireTimeStamp = Date.now() + 1000 * 60 * 60 * 24 * 1.5
+const myExpireTimeStamp = Date.now() + 1000 * 60 * 60 * 24 * 1
 obj.obj.nucleicInfo.expireTime = formatDate(myExpireTimeStamp)
 obj.obj.nucleicInfo.expireTimeStamp = myExpireTimeStamp.toString()
 
 //采样时间
 
-const mySampleTime = Date.now() - 1000 * 60 * 60 * 24 * 1.5
+const mySampleTime = Date.now() - 1000 * 60 * 60 * 24 * 1
 obj.obj.nucleicInfo.samplingTime = formatDate(mySampleTime)
 obj.obj.nucleicInfo.sampleTimestamp = mySampleTime.toString()
 
