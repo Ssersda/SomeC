8fa059be17a8d0d0db0c045f2c9914a3be99b26e
Collins Scott <107673661+Ssersda@users.noreply.github.com>
2022-10-02 10:03:46 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,14 +1,1 @@
-var body = $response.body;
-var obj = JSON.parse(body);
 
-//过期标志
-obj.obj.expireFlag=false
-
-obj.obj.nucleicInfo.expireHour = 24
-obj.obj.nucleicInfo.expireTime = "2022-10-15 22:34:32"
-obj.obj.nucleicInfo.expireTimeStamp='1665844472000'
-obj.obj.vaccineInfo.vaccinateCount=4
-body = JSON.stringify(obj);
-
-
-$done(body);
